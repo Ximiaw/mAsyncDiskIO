cc5261edd7bedeb35e24fb5d9325ef8fcac8c881
Ximiaw <m3311764705@outlook.com>
2026-05-28 18:32:17 +0800

    feat(core): 新增异步写入结果类
    引入 `async_result_write` 类，继承自 `async_result_base`，用于处理异步写入结果的功能。
    - 添加 `async_result_write` 类，继承自 `async_result_base`，在 `mAsyncDiskIO` 命名空间内定义。

---
+++ include/result_write.h
@@ -0,0 +1,14 @@
+#ifndef RESULT_WRITE_H
+#define RESULT_WRITE_H
+
+#include"result_base.h"
+
+namespace mAsyncDiskIO{
+
+    class async_result_write:async_result_base{
+        
+    };
+
+};
+
+#endif //RESULT_WRITE_H
